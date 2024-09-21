133b0c545e2bd8f4a0692311b5935616aab61df8
User <vova102204@gmail.com>
2024-09-21 17:06:36 +0700

    ОСМС - фикс багов

--- OSMS/lab1/main.py
+++ OSMS/lab1/main.py
@@ -29,30 +29,31 @@ def calculate_max_frequency(signal, Fs):
 
 def discretize_signal(f, Fs, duration):
     num_samples = int(Fs * duration)
-    time_points = np.linspace(0, duration, num_samples)
+    time_points = np.arange(num_samples) / Fs 
     discretize_signal = generate_signal(f, time_points)
     return time_points, discretize_signal
 
 
-def plot_spectrum(signal, Fs, title="Амплитудный спектр сигнала", subplot_pos=None):
-    frequencies = np.fft.fftfreq(signal.size, 1 / Fs)
-    spectrum = np.abs(np.fft.fft(signal))
-    
+def plot_spectrum(signal, Fs, title, subplot_pos=None):
+    dft_result = np.fft.fft(signal) 
+    magnitude = np.abs(dft_result) 
+
     if subplot_pos is not None:
         plt.subplot(subplot_pos)
     else:
         plt.figure(figsize=(12, 4))
 
-    plt.plot(frequencies, spectrum)
+    plt.plot(magnitude)
     plt.title(title)
-    plt.xlabel("Частота (Гц)")
+    plt.xlabel("Частота") 
     plt.ylabel("Амплитуда")
-    plt.grid(True)
-    
+    plt.grid(True)    
 
 def quantize_signal(signal, num_bits):
-    max_value = 2**(num_bits - 1) - 1
-    quantized_signal = np.round(np.clip(signal * max_value, -max_value, max_value)) / max_value
+    signal_range = np.max(signal) - np.min(signal)
+    quantization_levels = 2**num_bits-1
+    quantization_step = signal_range / quantization_levels
+    quantized_signal = np.round(signal / quantization_step) * quantization_step
     return quantized_signal
 
 
@@ -74,9 +75,9 @@ def main():
     print(f"Минимальная частота дискретизации (Котельников): {Fs_min} Гц")
 
     t2, discrete_signal = discretize_signal(f, Fs_min, duration)
-    plot_signal(t2, discrete_signal, f"Дискретизированный сигнал (Fs = {Fs_min} Гц)", 422)
+    plot_signal(t2, discrete_signal, f"Дискретизированный сигнал (Fs = {Fs_min} Гц)", 423)
 
-    plot_spectrum(discrete_signal, Fs_min, subplot_pos=423)
+    plot_spectrum(discrete_signal, Fs_min, "Амплитудный спектр 18ГЦ",subplot_pos=425)
     spectral_width = Fs_min
     print(f"\nШирина спектра: {spectral_width} Гц")
     memory_usage = discrete_signal.nbytes
@@ -85,7 +86,7 @@ def main():
     Fs_increased = 4 * Fs_min
     t3, discrete_signal_increased = discretize_signal(f, Fs_increased, duration)
     plot_signal(t3, discrete_signal_increased, f"Дискретизированный сигнал (Fs = {Fs_increased} Гц)", 424)
-    plot_spectrum(discrete_signal_increased, Fs_increased, subplot_pos=425)
+    plot_spectrum(discrete_signal_increased, Fs_increased, "Амплитудный спектр 72ГЦ",subplot_pos=426)
     print(f"\nШирина спектра: (Fs = {Fs_increased} Гц)")
     memory_usage_increased = discrete_signal_increased.nbytes
     print(f"Объем памяти для хранения массива (Fs = {Fs_increased} Гц): {memory_usage_increased} байт")
@@ -131,7 +132,7 @@ def main():
     plt.title('Амплитудный спектр прореженного сигнала')
     plt.xlabel('Частота (Гц)')
     plt.ylabel('Амплитуда')
-    plt.xlim(0, new_Fs/2)
+    plt.xlim(0, Fs/2)
     plt.legend()
     plt.grid(True)
 
@@ -141,14 +142,27 @@ def main():
     num_plots = len([3, 4, 5, 6])
     for i, num_bits in enumerate([3, 4, 5, 6], 1):
         quantized_signal = quantize_signal(discrete_signal, num_bits)
-        plot_spectrum(quantized_signal, Fs_min, title=f"Спектр сигнала после квантования ({num_bits} бит)",
+        plot_spectrum(quantized_signal, Fs_min, title=f"Спектр сигнала после квантования ({num_bits} бит (18 Гц))",
                      subplot_pos=num_plots * 100 + 10 + i)
         quantization_error = np.mean(np.abs(discrete_signal - quantized_signal))
-        print(f"Средняя ошибка квантования ({num_bits} бит): {quantization_error:.4f}")
+        print(f"Средняя ошибка квантования ({num_bits} бит (18 Гц)): {quantization_error:.4f}")
 
     plt.tight_layout()
     plt.show()
 
+    print()
+
+    plt.figure(figsize=(12, 11))
+    num_plots = len([3, 4, 5, 6])
+    for i, num_bits in enumerate([3, 4, 5, 6], 1):
+        quantized_signal_inc = quantize_signal(discrete_signal_increased, num_bits)
+        plot_spectrum(quantized_signal_inc, Fs_min, title=f"Спектр сигнала после квантования ({num_bits} бит (72 Гц))",
+                     subplot_pos=num_plots * 100 + 10 + i)
+        quantization_error = np.mean(np.abs(discrete_signal_increased - quantized_signal_inc))
+        print(f"Средняя ошибка квантования ({num_bits} бит (72 Гц)): {quantization_error:.4f}")
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     main()
