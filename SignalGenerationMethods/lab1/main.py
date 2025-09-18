fa98d4ca07b990f76968b004d46686ffe17e4a7a
Vladimir Ponomarenko <vova102204@gmail.com>
2025-09-18 16:13:59 +0700

    Методы формирования сигналов - лаб 4
    Добавлены arg(h), |h|, функция автокорреляции

---
+++ SignalGenerationMethods/lab1/main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+sigma = 1.0
+N = 5000
+
+hi = np.random.normal(0, sigma**2, N)   
+hq = np.random.normal(0, sigma, N)        
+
+h = hi + 1j * hq
+
+plt.figure(figsize=(10,4))
+
+plt.subplot(1,3,1)
+plt.hist(np.abs(h), bins=50, density=True)
+plt.title('|h|')
+
+plt.subplot(1,3,2)
+plt.hist(np.angle(h), bins=50, density=True)
+plt.title('arg(h)')
+
+acf = np.correlate(h, h, mode= 'full') / N
+lags  = np.arange(-N+1, N)
+
+plt.subplot(1,3,3)
+plt.plot(lags, np.abs(acf))
+plt.title('Autocorr |h|')
+
+plt.tight_layout()
+plt.show()
