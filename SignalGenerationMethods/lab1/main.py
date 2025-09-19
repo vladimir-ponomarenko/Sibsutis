cce922e3d104523045e4ce1785b3aee240f2d2cd
Vladimir Ponomarenko <vova102204@gmail.com>
2025-09-19 23:07:03 +0700

    Мобильные системы новых поколений - лаб 4

--- SignalGenerationMethods/lab1/main.py
+++ SignalGenerationMethods/lab1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sigma = 1.0
-N = 5000
+N = 8000
 
 hi = np.random.normal(0, sigma**2, N)   
 hq = np.random.normal(0, sigma, N)        
@@ -12,11 +12,11 @@ h = hi + 1j * hq
 plt.figure(figsize=(10,4))
 
 plt.subplot(1,3,1)
-plt.hist(np.abs(h), bins=50, density=True)
+plt.hist(np.abs(h), bins=80, density=True)
 plt.title('|h|')
 
 plt.subplot(1,3,2)
-plt.hist(np.angle(h), bins=50, density=True)
+plt.hist(np.angle(h), bins=80, density=True)
 plt.title('arg(h)')
 
 acf = np.correlate(h, h, mode= 'full') / N
@@ -24,7 +24,7 @@ lags  = np.arange(-N+1, N)
 
 plt.subplot(1,3,3)
 plt.plot(lags, np.abs(acf))
-plt.title('Autocorr |h|')
+plt.title('Autocorr h')
 
 plt.tight_layout()
 plt.show()
