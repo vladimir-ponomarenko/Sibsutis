5e7952c94f074219246c5adee091fe2362c94545
Vladimir Ponomarenko <vova102204@gmail.com>
2025-10-16 21:46:29 +0700

    Методы формирования сигналов - лаб 9

--- SignalGenerationMethods/lab6/main.py
+++ SignalGenerationMethods/lab6/main.py
@@ -30,9 +30,7 @@ ax1.set_zlabel('|H(f)|')
 ax1.set_title('H(f)')
 
 ht = ifft(h, axis=0)
-
 tao_us = (np.arange(N) * (1/BW)) * 1e6
-
 X_t, Y_t = np.meshgrid(cols, tao_us)
 Z_t = np.abs(ht)
 fig2 = plt.figure(figsize=(12, 8))
@@ -58,13 +56,10 @@ ax3.grid(True)
 
 
 tao_s = np.arange(N) * (1/BW)
-
-
 Nsn = h.shape[1]
 abs_ht_squared = np.abs(ht)**2
 sum_over_channels = np.sum(abs_ht_squared, axis=1)
 P_tao = sum_over_channels / Nsn
-
 peak_power = np.max(P_tao)
 threshold = peak_power * 10**(-25 / 10)
 significant_indices = np.where(P_tao > threshold)[0]
@@ -81,7 +76,27 @@ else:
     numerator_S = np.sum(((tao_s_filtered - mean_delay)**2) * P_tao_filtered)
     S = np.sqrt(numerator_S / sum_p)
     print("\n--- Рассчитанные параметры канала ---")
-    print(f"Среднее избыточное запаздывание (T_D): {T_D * 1e6:.4f} мкс")
-    print(f"СКО разброса задержек (S): {S * 1e6:.4f} мкс")
+    print(f"Задержка T_D: {T_D * 1e6:.4f} мкс")
+    print(f"СКО разброса задержек S: {S * 1e6:.4f} мкс")
+
+
+max_freq_dist = 40
+Bh_values = []
+freq_step = Fs / N
+delta_f_khz = (np.arange(max_freq_dist + 1) * freq_step) / 1000
+for m in range(max_freq_dist + 1):
+    h1 = h[0:N-m, :]
+    h2 = h[m:N, :]
+    product = np.conj(h1) * h2
+    correlation = np.mean(product)
+    Bh_values.append(correlation)
+
+fig4, ax4 = plt.subplots(figsize=(12, 8))
+
+ax4.plot(delta_f_khz, np.abs(Bh_values), linestyle='-')
+ax4.set_xlabel('Δf, кГц')
+ax4.set_ylabel('|Bh(Δf)|')
+ax4.set_title('Частотная корреляционная функция')
+ax4.grid(True)
 
 plt.show()
